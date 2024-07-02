ef6997867ddad00e366414c61a5f0b78b8e4d064
duke <dukebs1128@gmail.com>
2024-07-02 17:44:10 +0900

    feat : update 24.07.02

--- FamTec/FamTec/FamTec/Shared/Server/DTO/Place/PlacePerm.cs
+++ FamTec/FamTec/FamTec/Shared/Server/DTO/Place/PlacePerm.cs
@@ -12,33 +12,33 @@ namespace FamTec.Shared.Server.DTO.Place
         public int Id { get; set; }
 
         [Display(Name = "기계설비 권한")]
-        public sbyte? PermMachine { get; set; } = 0;
+        public sbyte? PermMachine { get; set; }
 
         [Display(Name = "승강설비 권한")]
-        public sbyte? PermLift { get; set; } = 0;
+        public sbyte? PermLift { get; set; }
 
         [Display(Name = "소방설비 권한")]
-        public sbyte? PermFire { get; set; } = 0;
+        public sbyte? PermFire { get; set; }
 
         [Display(Name = "건축설비 권한")]
-        public sbyte? PermConstruct { get; set; } = 0;
+        public sbyte? PermConstruct { get; set; }
 
         [Display(Name = "통신설비 권한")]
-        public sbyte? PermNetwork { get; set; } = 0;
+        public sbyte? PermNetwork { get; set; }
 
         [Display(Name = "미화설비 권한")]
-        public sbyte? PermBeauty { get; set; } = 0;
+        public sbyte? PermBeauty { get; set; }
 
         [Display(Name = "보안설비 권한")]
-        public sbyte? PermSecurity { get; set; } = 0;
+        public sbyte? PermSecurity { get; set; }
 
         [Display(Name = "자재 권한")]
-        public sbyte? PermMaterial { get; set; } = 0;
+        public sbyte? PermMaterial { get; set; }
 
         [Display(Name = "에너지 권한")]
-        public sbyte? PermEnergy { get; set; } = 0;
+        public sbyte? PermEnergy { get; set; }
 
         [Display(Name = "민원 권한")]
-        public sbyte? PermVoc { get; set; } = 0;
+        public sbyte? PermVoc { get; set; }
     }
 }
