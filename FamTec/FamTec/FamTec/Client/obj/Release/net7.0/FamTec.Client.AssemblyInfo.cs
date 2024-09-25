65314b31e5c8a5b80db496de72f90f26e99f7767
kyw <rladyddn258@gmail.com>
2024-09-25 13:14:42 +0900

    200번 수정

--- FamTec/FamTec/FamTec/Client/obj/Release/net7.0/FamTec.Client.AssemblyInfo.cs
+++ FamTec/FamTec/FamTec/Client/obj/Release/net7.0/FamTec.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a9a7d6820f47e18d900ed6e8e70b8dce5d4965")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19d6c2072b6fdb5d2ebf3323f4654b9bb84ff5cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
