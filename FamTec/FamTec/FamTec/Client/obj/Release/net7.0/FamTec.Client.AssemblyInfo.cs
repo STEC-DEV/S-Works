5f8315a61814d9112960f8ca37b484431f1e4e76
kyw <rladyddn258@gmail.com>
2024-10-14 14:09:33 +0900

    중간커밋

--- FamTec/FamTec/FamTec/Client/obj/Release/net7.0/FamTec.Client.AssemblyInfo.cs
+++ FamTec/FamTec/FamTec/Client/obj/Release/net7.0/FamTec.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4f2b74b7c2002970bf432b33138fb9535a8bf49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+765e6ac0cb53d5333fdaea739edc94e4329c0e0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
