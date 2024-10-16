73d4beec52c5b8ef8d499c4027916abb83d15f9c
kyw <rladyddn258@gmail.com>
2024-10-16 21:05:56 +0900

    중간커밋2

--- FamTec/FamTec/FamTec/Server/obj/Release/net7.0/FamTec.Server.AssemblyInfo.cs
+++ FamTec/FamTec/FamTec/Server/obj/Release/net7.0/FamTec.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41b64787ad8b8f9ec3951517ada4dc8b76e39698")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77a3d264c8d07bc6afe1f7be6cfc4d5d81e04bff")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
