53e5d2445d779f2e72db10083e3285ed2d57db10
kyw <rladyddn258@gmail.com>
2024-10-21 15:33:13 +0900

    merge2

--- FamTec/FamTec/FamTec/Server/obj/Release/net7.0/linux-x64/FamTec.Server.AssemblyInfo.cs
+++ FamTec/FamTec/FamTec/Server/obj/Release/net7.0/linux-x64/FamTec.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91acd308bb95e9210eeae577b490797a4084cb48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6774ec3e31d814d5adb311b554c7a0c4e1587c59")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
