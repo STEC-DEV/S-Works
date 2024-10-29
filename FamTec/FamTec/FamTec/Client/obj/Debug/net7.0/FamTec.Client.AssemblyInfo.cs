53db72532093227a961077e15da7843591facec1
duke <dukebs1128@gmail.com>
2024-10-29 17:12:35 +0900

    feat : update 이미지 확장자 수정 gif 제외 bmp 추가

--- FamTec/FamTec/FamTec/Client/obj/Debug/net7.0/FamTec.Client.AssemblyInfo.cs
+++ FamTec/FamTec/FamTec/Client/obj/Debug/net7.0/FamTec.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d03d3ec874aebc559b40ed03fdb63174996660f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03f312b828de30970da68815d27650eddfeca807")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
