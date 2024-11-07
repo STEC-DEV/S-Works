091cfdff7c8dab61fee918050c5774b12c987657
kyw <rladyddn258@gmail.com>
2024-11-07 11:00:25 +0900

    중간커밋

--- FamTec/FamTec/FamTec/Server/obj/Release/net7.0/linux-x64/FamTec.Server.AssemblyInfo.cs
+++ FamTec/FamTec/FamTec/Server/obj/Release/net7.0/linux-x64/FamTec.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e359c1beec4cc4607f2e9fddd1ecddfb240d0ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25db0818ff5babf9076d4b2aec3239655958b579")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FamTec/FamTec/FamTec/Shared/obj/Release/net7.0/FamTec.Shared.AssemblyInfo.cs
+++ FamTec/FamTec/FamTec/Shared/obj/Release/net7.0/FamTec.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamTec.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8762603bd2084580d3fa7f7fb21f1f2d87add7db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25db0818ff5babf9076d4b2aec3239655958b579")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamTec.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamTec.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
