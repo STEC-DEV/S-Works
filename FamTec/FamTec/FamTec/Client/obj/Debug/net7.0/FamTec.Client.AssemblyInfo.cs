fdb44134c0026ba9a3443e62823a66f544d0534c
kyw <rladyddn258@gmail.com>
2024-11-20 08:18:29 +0900

    중간커밋

--- FamTec/FamTec/FamTec/Client/obj/Debug/net7.0/FamTec.Client.AssemblyInfo.cs
+++ FamTec/FamTec/FamTec/Client/obj/Debug/net7.0/FamTec.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f8f5fc5acc82a5b7137c6644dfcb012a6afa8c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60ea19a198d5124e9cb273dc23b18b40d005bb47")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
