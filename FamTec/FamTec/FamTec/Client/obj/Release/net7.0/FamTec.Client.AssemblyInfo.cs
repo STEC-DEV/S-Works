45cd06d613a38a888c73f54eb1bd2c90829a51ea
kyw <rladyddn258@gmail.com>
2025-03-07 14:33:37 +0900

    commit

--- FamTec/FamTec/FamTec/Client/obj/Release/net7.0/FamTec.Client.AssemblyInfo.cs
+++ FamTec/FamTec/FamTec/Client/obj/Release/net7.0/FamTec.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.4.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.4+0b59ebcedea7a9a57eac752a93589bc24df3ffb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.4+6d8e42a1b316bc8031491c74aa2637a59300734e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.4.0.0")]
